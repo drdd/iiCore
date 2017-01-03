86ae5bb08d0f72c6001fed84d3c5102ad378345d
Nickolay <mymailbyte@gmail.com>
2017-01-03 17:19:38 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-# Start apache
-/usr/sbin/apache2 -D FOREGROUND
+chown www-data:www-data /app -R
+
+if [ "$ALLOW_OVERRIDE" = "**False**" ]; then
+    unset ALLOW_OVERRIDE
+else
+    sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
+    a2enmod rewrite
+fi
+
+source /etc/apache2/envvars
+tail -F /var/log/apache2/* &
+exec apache2 -D FOREGROUND
